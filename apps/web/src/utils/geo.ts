51bc52384ae67b20c4d1b2fed9135d8077b015a5
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-10-29 14:33:12 +0200

    fix import

--- apps/web/src/utils/geo.ts
+++ apps/web/src/utils/geo.ts
@@ -1,5 +1,5 @@
 import classifyPoint from 'robust-point-in-polygon'
-import { IPolygon } from '../../../data/src/interfaces'
+import { IPolygon } from '../../../packages/magen_common_ts/src/interfaces'
 
 export function pointInPolygon(polygon: IPolygon, point: [number, number]) {
     return classifyPoint(polygon, point) != 1 // 1 means it's outside
