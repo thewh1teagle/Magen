6ffd8e0aa1183cc6fa8c44da85c516b6e46d5f78
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-11-01 00:48:34 +0200

    update cron job

--- ws_srv/cron_job.sh
+++ ws_srv/cron_job.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# cd into script folder
+cd "$(dirname "$0")";
+
 # cleanup exited docker containers
 EXITED_CONTAINERS=$(docker ps -a | grep Exited | awk '{ print $1 }')
 if [ -z "$EXITED_CONTAINERS" ]
@@ -11,7 +15,7 @@ fi
 # renew certbot certificate
 iptables-save > iptables-rules-backup # save iptables
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT # temporarily allow anything on port 80
-docker compose -f /root/orefmap/server/docker-compose.yaml run --rm certbot
-docker compose -f /root/orefmap/server/docker-compose.yaml exec nginx nginx -s reload
+docker compose run --rm certbot
+docker compose exec nginx nginx -s reload
 iptables-restore < iptables-rules-backup # restore iptables
 rm -f iptables-rules-backup # remove iptables backup
